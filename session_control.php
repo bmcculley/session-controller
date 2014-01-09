d9745a1d1d9722390f3c98c177628f3060e9cb17
bmcculley <bmcculley@users.noreply.github.com>
2014-01-09 07:21:16 -0500

    fixing indentation issues.

--- session_control.php
+++ session_control.php
@@ -12,8 +12,8 @@ class SessionControl {
 	protected $table = 'sessioncontrol';
 
 	/**
-     * Initialize
-     */
+	 * Initialize
+	 */
 	function SessionControl() {
 		session_start();
 		include_once('config.php');
@@ -21,15 +21,15 @@ class SessionControl {
 	}
 
 	/**
-     * Start a new connection
-     */
+	 * Start a new connection
+	 */
 	private function db_connect($host, $user, $pass, $db) {
 		$this->con = mysqli_connect($host, $user, $pass, $db);
 	}
 
 	/**
-     * Create a new session
-     */
+	 * Create a new session
+	 */
 	function start_session($ip) {
 		$vid = md5($ip);
 		if ($this->store_session($vid, $ip)) {
@@ -38,8 +38,8 @@ class SessionControl {
 	}
 
 	/**
-     * Store new session in database
-     */
+	 * Store new session in database
+	 */
 	private function store_session($vid, $ip) {
 		$sql = sprintf("REPLACE INTO %s VALUES('%s', '%s', '%s', '%s')",
         			   $this->table,
@@ -47,52 +47,52 @@ class SessionControl {
                        $this->con->escape_string($vid),
                        $this->con->escape_string($ip),
                        time());
-        return $this->con->query($sql);
+        	return $this->con->query($sql);
 	}
 
 	/**
-     * Determine if visitor has a current session
-     */
+	 * Determine if visitor has a current session
+	 */
 	function get_session($vid,$ip) {
 		$sql = sprintf("SELECT * FROM %s WHERE visitor_id = '%s' AND ip_address = '%s'", $this->table, $this->con->escape_string($vid), $this->con->escape_string($ip));
-        if ($result = $this->con->query($sql)) {
-        	return true;
-        }
-        else {
-        	return false;
-        }
+        	if ($result = $this->con->query($sql)) {
+        		return true;
+        	}
+        	else {
+        		return false;
+        	}
 	}
 
 	/**
-     * Update session timestamp on visitor activity
-     */
+	 * Update session timestamp on visitor activity
+	 */
 	function update_activity($vid,$ip) {
 		$sql = sprintf("UPDATE %s SET timestamp = %s WHERE visitor_id = '%s' AND ip_address = '%s'", $this->table, time(), $this->con->escape_string($vid), $this->con->escape_string($ip));
 		if ($result = $this->con->query($sql)) {
-        	return true;
-        }
-        else {
-        	return false;
-        }
+        		return true;
+        	}
+        	else {
+        		return false;
+        	}
 	}
 
 	/**
-     * Destroy a current session by visitor id
-     */
+	 * Destroy a current session by visitor id
+	 */
 	function clear_session($vid) {
 		$sql = sprintf("DELETE FROM %s WHERE `visitor_id` = '%s'", $this->table, $this->con->escape_string($vid));
 		unset($_COOKIE['sess']);
-        return $this->con->query($sql);
+        	return $this->con->query($sql);
 	}
 
 	/**
-     * Remove old sessions
-     * Use a cron job to clean out old sessions
-     */
+	 * Remove old sessions
+	 * Use a cron job to clean out old sessions
+	 */
 	function clear_old() {
 		$limit = time() - (3600 * 24);
-        $sql = sprintf("DELETE FROM %s WHERE timestamp < %s", $this->table, $limit);
-        return $this->con->query($sql);
+		$sql = sprintf("DELETE FROM %s WHERE timestamp < %s", $this->table, $limit);
+	  	return $this->con->query($sql);
 	}
 }
 ?>
